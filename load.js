0dd3d7a4baeb0b5f00cafb7b302791f25a6669fd
Pete Silvanto <silvanto.pete@gmail.com>
2024-11-16 14:11:59 +0700

    formatting

--- load.js
+++ load.js
@@ -32,8 +32,6 @@ class Load extends Phaser.Scene {
     }
 
     create() {
-        
-        
         console.log(this.textures.list);
         music = this.sound.add('backGroundMusic');
         music.play();
